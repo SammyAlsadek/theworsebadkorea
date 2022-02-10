60617c905e642ab3ffd06c8adb598fab99e4e672
cpprogues <joshgarde@gmail.com>
2022-02-09 17:30:52 -0800

    aaaaaaaaaaa

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import re
 from bs4 import BeautifulSoup
 import requests
 
-frontier = set() # going to
+frontier = set(['https://www.telemundo.com/']) # going to
 explored = set() # already did
 
 url = 'https://www.telemundo.com/'
@@ -11,17 +11,25 @@ url = 'https://www.telemundo.com/'
 domain_re = re.compile(r'(https?)?:\/\/(.*?)(/.*)')
 domain = domain_re.match(url).group(2)
 
-r = requests.get(url)
-if r.ok:
-    soup = BeautifulSoup(r.text, 'html.parser')
-    all_links = [link.get('href') for link in soup.find_all('a')]
-    internal_links = []
+while len(frontier) > 0:
+    url = frontier.pop()
+    print('URL: {0}'.format(url))
 
-    for link in all_links:
-        match = domain_re.match(link)
-        if match is None:
-            internal_links.append(link)
-        elif match.group(2) == domain:
-            internal_links.append(link)
+    r = requests.get(url)
+    if r.ok:
+        soup = BeautifulSoup(r.text, 'html.parser')
+        all_links = [link.get('href') for link in soup.find_all('a')]
+        internal_links = []
 
-    explored.add(url)
+        for link in all_links:
+            match = domain_re.match(link)
+            if match is None:
+                internal_links.append('://{0}/{1}'.format(domain, link))
+            elif match.group(2) == domain:
+                internal_links.append(link)
+
+        explored.add(url)
+
+        for link in internal_links:
+            if link not in explored:
+                frontier.add(link)
